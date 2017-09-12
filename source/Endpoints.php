ba815f38c132232688a3bd00d136af14577c6f74
Kolya Korobochkin <korobochkin@setka.io>
2017-09-12 12:44:33 +0300

    endpoints doc

--- source/Endpoints.php
+++ source/Endpoints.php
@@ -3,15 +3,37 @@ namespace Setka\WorkflowSDK;
 
 class Endpoints
 {
+    /**
+     * %1$s - space slug
+     */
     const CATEGORIES = '/eapi/v3/%1$s/categories.json';
 
+    /**
+     * %1$s - space slug
+     * %2$s - category ID
+     */
     const CATEGORY = '/eapi/v3/%1$s/categories/%2$s.json';
 
+    /**
+     * %1$s - space slug
+     * %2$s - ticket ID
+     */
     const TICKETS_PUBLISH = '/eapi/v3/%1$s/tickets/%2$s/publish.json';
 
+    /**
+     * %1$s - space slug
+     */
     const TICKETS_SYNC_ANALYTICS = '/eapi/v3/%1$s/tickets/sync_analytics.json';
 
+    /**
+     * %1$s - space slug
+     * %2$s - ticket ID
+     */
     const TICKETS_UNPUBLISH = '/eapi/v3/%1$s/tickets/%2$s/unpublish.json';
 
+    /**
+     * %1$s - space slug
+     * %2$s - ticket ID
+     */
     const TICKET = '/eapi/v3/%1$s/tickets/%2$s.json';
 }
