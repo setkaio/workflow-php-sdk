04015dd2405e498c1f6d2a73dd55f8df1299370d
Kolya Korobochkin <korobochkin@setka.io>
2017-09-21 18:24:42 +0300

    sync analytics

--- source/Actions/Tickets/SyncTicketAnalyticsAction.php
+++ source/Actions/Tickets/SyncTicketAnalyticsAction.php
@@ -3,10 +3,56 @@ namespace Setka\WorkflowSDK\Actions\Tickets;
 
 use Setka\WorkflowSDK\Actions\AbstractAction;
 use Setka\WorkflowSDK\Endpoints;
+use Setka\WorkflowSDK\Entities\TicketEntity;
+use Setka\WorkflowSDK\Exceptions\UnauthorizedException;
+use Setka\WorkflowSDK\Exceptions\UnknownResponseException;
+use Setka\WorkflowSDK\Exceptions\UnprocessableEntityException;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SyncTicketAnalyticsAction extends AbstractAction
 {
+    /**
+     * Handle response.
+     *
+     * @throws UnauthorizedException If token missed or invalid.
+     * @throws UnprocessableEntityException If something your your request was wrong (or ticket already published).
+     * @throws UnknownResponseException If API returns unknown HTTP status code.
+     *
+     * @return TicketEntity[] If response was successful.
+     */
+    public function handleResponse()
+    {
+        switch($this->getResponse()->getStatusCode()) {
+            case 200:
+                $data = $this->decodeResponse();
+                $entities = array();
+
+                foreach($data as $a)
+                {
+                    $b = new TicketEntity();
+                    $b
+                        ->setId($a['id'])
+                        ->setViewPostUrl($a['view_post_url'])
+                        ->setViewsCount($a['views_count'])
+                        ->setCommentsCount($a['comments_count']);
+
+                    $entities[] = $b;
+                }
+
+                return $entities;
+
+            case 401:
+                throw new UnauthorizedException();
+
+            case 422:
+                $data = $this->decodeResponse();
+                throw new UnprocessableEntityException($data['message']);
+
+            default:
+                throw new UnknownResponseException();
+        }
+    }
+
     /**
      * @inheritdoc
      */
