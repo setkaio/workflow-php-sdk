f4602e9dc355af2f2e2ae8b33eac74fb91c43a9e
Kolya Korobochkin <korobochkin@setka.io>
2017-09-21 18:35:25 +0300

    unpublish

--- source/Actions/Tickets/UnpublishTicketAction.php
+++ source/Actions/Tickets/UnpublishTicketAction.php
@@ -3,10 +3,59 @@ namespace Setka\WorkflowSDK\Actions\Tickets;
 
 use Setka\WorkflowSDK\Actions\AbstractAction;
 use Setka\WorkflowSDK\Endpoints;
+use Setka\WorkflowSDK\Entities\TicketEntity;
+use Setka\WorkflowSDK\Exceptions\NotFoundException;
+use Setka\WorkflowSDK\Exceptions\UnauthorizedException;
+use Setka\WorkflowSDK\Exceptions\UnknownResponseException;
+use Setka\WorkflowSDK\Exceptions\UnprocessableEntityException;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UnpublishTicketAction extends AbstractAction
 {
+    /**
+     * Handle response.
+     *
+     * @throws UnauthorizedException If token missed or invalid.
+     * @throws NotFoundException If ticket not found.
+     * @throws UnprocessableEntityException If something your your request was wrong (or ticket already published).
+     * @throws UnknownResponseException If API returns unknown HTTP status code.
+     *
+     * @return TicketEntity If response was successful.
+     */
+    public function handleResponse()
+    {
+        switch($this->getResponse()->getStatusCode()) {
+            case 200:
+                $entity = new TicketEntity();
+                $data = $this->decodeResponse();
+                $entity
+                    ->setId($data['id'])
+                    ->setTitle($data['title'])
+                    ->setCategoryId($data['category_id'])
+                    ->setState($data['state'])
+                    ->setPublishedAt($data['published_at'])
+                    ->setViewPostUrl($data['view_post_url'])
+                    ->setEditPostUrl($data['edit_post_url'])
+                    ->setViewsCount($data['views_count'])
+                    ->setCommentsCount($data['comments_count']);
+
+                return $entity;
+
+            case 401:
+                throw new UnauthorizedException();
+
+            case 404:
+                throw new NotFoundException();
+
+            case 422:
+                $data = $this->decodeResponse();
+                throw new UnprocessableEntityException($data['message']);
+
+            default:
+                throw new UnknownResponseException();
+        }
+    }
+
     /**
      * @inheritdoc
      */
---
+++ tests/Data/Responses/UnpublishTicketActionDataSet.php
@@ -0,0 +1,128 @@
+<?php
+namespace Setka\WorkflowSDK\Tests\Data\Responses;
+
+use Setka\WorkflowSDK\Entities\TicketEntity;
+use Setka\WorkflowSDK\Exceptions\NotFoundException;
+use Setka\WorkflowSDK\Exceptions\UnauthorizedException;
+use Setka\WorkflowSDK\Exceptions\UnprocessableEntityException;
+use Setka\WorkflowSDK\Tests\Data\AbstractAssociativeDataSet;
+
+class UnpublishTicketActionDataSet extends AbstractAssociativeDataSet
+{
+    public function __construct()
+    {
+        $variants = array();
+
+        $variants['1.'] = array(
+            array(
+                'space' => 'test-space',
+                'token' => 'P9mYAXprVQBG9PFQwLiSzv8VyUbfXt6cP9mYAXprVQBG9PFQwL',
+                'id'  => '123456',
+            ),
+            array(
+                'http_code' => 200,
+                'http_body' => array(
+                    'id' => '123456',
+                    'title' => 'Test Title',
+                    'category_id' => '78910',
+                    'state' => 'assigned',
+                    'published_at' => null,
+                    'view_post_url' => 'https://test-site.com/111/',
+                    'edit_post_url' => 'https://test-site.com/admin/edit-post/111/',
+                    'views_count' => 0,
+                    'comments_count' => 0,
+                ),
+                'handle_expect' => TicketEntity::class,
+            ),
+        );
+
+        $variants['1.1'] = array(
+            array(
+                'space' => 'test-space',
+                'token' => 'P9mYAXprVQBG9PFQwLiSzv8VyUbfXt6cP9mYAXprVQBG9PFQwL',
+                'id'  => '123456',
+            ),
+            array(
+                'http_code' => 200,
+                'http_body' => array(
+                    'id' => '123456',
+                    'title' => 'Test Title',
+                    'category_id' => '78910',
+                    'state' => 'assigned',
+                    'published_at' => null,
+                    'view_post_url' => 'https://test-site.com/111/',
+                    'edit_post_url' => 'https://test-site.com/admin/edit-post/111/',
+                    'views_count' => 0,
+                    'comments_count' => 0,
+                ),
+                'handle_expect' => TicketEntity::class,
+            ),
+        );
+
+        $variants['1.2'] = array(
+            array(
+                'space' => 'test-space',
+                'token' => 'P9mYAXprVQBG9PFQwLiSzv8VyUbfXt6cP9mYAXprVQBG9PFQwL',
+                'id'  => '123456',
+            ),
+            array(
+                'http_code' => 200,
+                'http_body' => array(
+                    'id' => '123456',
+                    'title' => 'Test Title',
+                    'category_id' => 78910,
+                    'state' => 'assigned',
+                    'published_at' => null,
+                    'view_post_url' => 'https://test-site.com/111/',
+                    'edit_post_url' => 'https://test-site.com/admin/edit-post/111/',
+                    'views_count' => null,
+                    'comments_count' => null,
+                ),
+                'handle_expect' => TicketEntity::class,
+            ),
+        );
+
+        $variants['2.'] = array(
+            array(
+                'space' => 'test-space',
+                'token' => '',
+                'id'  => '123456',
+            ),
+            array(
+                'http_code' => 401,
+                'http_body' => array(),
+                'handle_expect' => UnauthorizedException::class,
+            ),
+        );
+
+        $variants['3.'] = array(
+            array(
+                'space' => 'test-space',
+                'token' => 'P9mYAXprVQBG9PFQwLiSzv8VyUbfXt6cP9mYAXprVQBG9PFQwL',
+                'id'  => 'NOT_EXISTS_ID',
+            ),
+            array(
+                'http_code' => 404,
+                'http_body' => array(),
+                'handle_expect' => NotFoundException::class,
+            ),
+        );
+
+        $variants['4.'] = array(
+            array(
+                'space' => 'test-space',
+                'token' => 'P9mYAXprVQBG9PFQwLiSzv8VyUbfXt6cP9mYAXprVQBG9PFQwL',
+                'id'  => '123456',
+            ),
+            array(
+                'http_code' => 422,
+                'http_body' => array(
+                    'message' => 'Ticket is not published',
+                ),
+                'handle_expect' => UnprocessableEntityException::class,
+            ),
+        );
+
+        $this->variants = $variants;
+    }
+}
